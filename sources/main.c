cb2c67bf26088cac66a7ec03e50f260ce46bb951
¨Nikolai <nzharkev@student.hive.fi>
2024-10-29 16:24:15 +0200

    first commit

--- sources/main.c
+++ sources/main.c
@@ -6,13 +6,15 @@
 /*   By: nzharkev <nzharkev@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 15:40:55 by nzharkev          #+#    #+#             */
-/*   Updated: 2024/10/26 15:42:29 by nzharkev         ###   ########.fr       */
+/*   Updated: 2024/10/29 16:22:32 by nzharkev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **)
 {
+	(void)argc;
+	(void)argv;
 	return (0);
 }
