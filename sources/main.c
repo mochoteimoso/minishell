7604ceaae8b5725b27c2def7521837daf92b1e52
¨Nikolai <nzharkev@student.hive.fi>
2024-10-29 17:54:54 +0200

    Add prompt appears and history

--- sources/main.c
+++ sources/main.c
@@ -6,15 +6,33 @@
 /*   By: nzharkev <nzharkev@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 15:40:55 by nzharkev          #+#    #+#             */
-/*   Updated: 2024/10/29 16:22:32 by nzharkev         ###   ########.fr       */
+/*   Updated: 2024/10/29 17:25:33 by nzharkev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	main(int argc, char **argv, char **)
+static int user_prompt(int exit_c, char **envp)
 {
+	char	*input;
+	(void)envp;
+	while (1)
+	{
+		input = readline("minishell> ");
+		if (input == NULL)
+			break ;
+		if (input && *input)
+			add_history(input);
+	}
+	return (exit_c);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	int	exit_c;
+
+	exit_c = 0;
 	(void)argc;
 	(void)argv;
-	return (0);
+	return (user_prompt(exit_c, envp));
 }
