f2df53fe6e511caafc74df2c5e3f8d21a18e4efa
Heidi E <henbuska@student.hive.fi>
2024-10-31 15:37:44 +0200

    Add functions for creating  a new tokens linked list

--- sources/parser.c
+++ sources/parser.c
@@ -6,10 +6,62 @@
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 16:26:26 by henbuska          #+#    #+#             */
-/*   Updated: 2024/10/29 18:56:09 by henbuska         ###   ########.fr       */
+/*   Updated: 2024/10/31 15:36:02 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+typedef struct s_tokens
+{
+	t_tok_type		type;
+	char			*content;
+	struct s_tokens	*next;
+}	t_tokens;
+
+typedef enum e_tok_type
+{
+	WORD,
+	PIPE,
+	APPEND,
+	REDIRECT_IN,    // redirection '<'
+	REDIRECT_OUT,   // redirection '>'
+	ENV_VAR,
+	HEREDOC,
+	DELIMITER
+} t_tok_type;
 
 
+void	add_token(t_tokens **tokens, t_tokens *new)
+{
+	t_tokens	*temp;
 
+	if (!tokens || !new)
+		return ;
+	if (!(*tokens))
+	{
+		*tokens = new;
+		return ;
+	}
+	temp = *tokens;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
+}
+
+t_tokens	*new_token(void *content)
+{
+	t_tokens	*new;
+
+	new = (t_tokens *)malloc(sizeof(t_tokens));
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
+
+int	tokenizer(char *line)
+{
+	
+}
