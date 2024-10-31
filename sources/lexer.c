114a048ab192129708af8cb7985a5252bccb0dc5
Heidi E <henbuska@student.hive.fi>
2024-10-31 15:36:54 +0200

    Rename str to line

--- sources/lexer.c
+++ sources/lexer.c
@@ -6,24 +6,24 @@
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:09:08 by henbuska          #+#    #+#             */
-/*   Updated: 2024/10/31 12:52:08 by henbuska         ###   ########.fr       */
+/*   Updated: 2024/10/31 15:03:33 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	check_line_syntax(char *str);
-int	check_quotes(char *str);
-int	check_pipes(char *str);
-int	check_consecutive_oper(char *str);
+int	check_line_syntax(char *line);
+int	check_quotes(char *line);
+int	check_pipes(char *line);
+int	check_consecutive_oper(char *line);
 
-int	check_line_syntax(char *str)
+int	check_line_syntax(char *line)
 {
-	if (!*str)
+	if (!*line)
 		return (1);
-	if (check_quotes(str))
+	if (check_quotes(line))
 		return (1);
-	if (check_pipes(str))
+	if (check_pipes(line))
 		return (1);
-	if (str == ';' || str == '\\')
+	if (line == ';' || line == '\\')
 	{
 		printf("Invalid syntax");
 		return (1);
@@ -32,20 +32,20 @@ int	check_line_syntax(char *str)
 		return (0);
 }
 
-int	check_quotes(char *str)
+int	check_quotes(char *line)
 {
 	int	in_single_quote;
 	int	in_double_quote;
 
 	in_single_quote = 0;
 	in_double_quote = 0;
-	while (*str)
+	while (*line)
 	{
-		if (*str == '\'' && in_double_quote)
+		if (*line == '\'' && in_double_quote)
 			in_single_quote = !in_single_quote;
-		else if (*str == '"' && in_single_quote)
+		else if (*line == '"' && in_single_quote)
 			in_double_quote = !in_double_quote;
-		str++;
+		line++;
 	}
 	if (in_double_quote || in_single_quote)
 		return (1);
@@ -53,54 +53,54 @@ int	check_quotes(char *str)
 		return (0);	
 }
 
-int	check_pipes(char *str)
+int	check_pipes(char *line)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] == ' ')
+	while (line[i] == ' ')
 		i++;
-	if (str[i] == '|')
+	if (line[i] == '|')
 	{
-		printf("syntax error near unexpected token %c\n", str[i]);
+		printf("syntax error near unexpected token %c\n", line[i]);
 		return (1);
 	}
-	if (check_consecutive_oper(str))
+	if (check_consecutive_oper(line))
 	{
-		printf("syntax error near unexpected token %c\n", str[i]);
+		printf("syntax error near unexpected token %c\n", line[i]);
 		return (1);
 	}
-	while (str[i])
+	while (line[i])
 		i++;
-	while (i >= 0 && str[i] == ' ')
+	while (i >= 0 && line[i] == ' ')
 		i--;
-	if (str[i] == '|')
+	if (line[i] == '|')
 	{
-		printf("syntax error near unexpected token %c\n", str[i]);
+		printf("syntax error near unexpected token %c\n", line[i]);
 		return (1);
 	}
 	return (0);
 }
 
-int	check_consecutive_oper(char *str)
+int	check_consecutive_oper(char *line)
 {
 	int	i;
 	int	pipe_found;
 
 	i = 0;
 	pipe_found = 0;
-	while (str[i])
+	while (line[i])
 	{
-		if (str[i] == '|')
+		if (line[i] == '|')
 		{
 			if (pipe_found)
 			{
-				printf("syntax error near unexpected token %c\n", str[i]);
+				printf("syntax error near unexpected token %c\n", line[i]);
 				return (1);
 			}
 			pipe_found = 1;
 		}
-		else if (str[i] == ' ')
+		else if (line[i] == ' ')
 			pipe_found = 0;
 		i++;
 	}
