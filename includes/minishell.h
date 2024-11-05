a8021122e202951513555c82c70fbfb2e51d1726
Heidi E <henbuska@student.hive.fi>
2024-11-05 17:36:25 +0200

    Add struct for general stuff and add variables to command struct

--- includes/minishell.h
+++ includes/minishell.h
@@ -20,11 +20,21 @@ typedef struct s_command
 	int		redirect_type; //??
 	char	**env_vars; //??
 	int		env_var_count; //??
-	bool	append;	
+	char	*append;
 	bool	heredoc;
-	char	*heredoc_delim;	
+	char	*heredoc_delim;
+	char	*heredoc_content;
+	int		exit_status;
 }	t_command;
 
+
+// struct to store general information
+
+typedef struct s_mini
+{
+	char	**split_input;
+
+} t_mini;
 
 // enum of token types
 
@@ -41,16 +51,20 @@ typedef enum e_tok_type
 } t_tok_type;
 
 
-int	validate_input_syntax(char *line);
-int	is_in_quotes(char *line, int i);
-int	check_consecutive_pipes(char *line);
-int	check_pipes(char *line);
-int	check_redirects(char *line);
-int	validate_redirect(char *line, int *i, char *type);
-int	count_pipes(char *line);
+int		validate_input_syntax(char *line);
+int		is_in_quotes(char *line, int i);
+int		check_consecutive_pipes(char *line);
+int		check_pipes(char *line);
+int		check_redirects(char *line);
+int		validate_redirect(char *line, int *i, char *type);
+int		count_pipes(char *line);
 t_command *initialize_cmd_array(int count);
 void	add_single_command(t_command *cmd, char *command);
 void	add_cmd_to_array(t_command *cmds, int *count, int size, char *command);
 void	handle_single_command(char *line);
+char	**split_input_by_pipes(char *line, t_mini *mini);
+char	**split_input_by_pipes(char *line, t_mini *mini);
+int		parse_input_segments(t_mini *mini, t_command *cmds);
+int		parse_and_validate_input(char *line, t_mini *mini);
 
 #endif
