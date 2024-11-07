47ab433da24cfe111c621799087b03df41a5c66e
Heidi E <henbuska@student.hive.fi>
2024-11-07 15:56:10 +0200

    Move struct initialization and change order of function calls in parsing function

--- sources/main.c
+++ sources/main.c
@@ -6,26 +6,32 @@
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 15:40:55 by nzharkev          #+#    #+#             */
-/*   Updated: 2024/11/06 18:08:58 by henbuska         ###   ########.fr       */
+/*   Updated: 2024/11/07 15:52:02 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	parse_and_validate_input(char *input, t_shell *sh, t_command **cmds)
+int	parse_and_validate_input(char *input, t_shell *sh)
 {
 	if (validate_input_syntax(input))
 		return (1);
+	if (prepare_command_structs(sh, input))
+		return (1);
 	if (split_input_by_pipes(input, sh))
 		return (1);
-	if (prepare_command_structs(input, cmds))
-		return (1);
-	printf("mini->split_input[0]: %s\n", sh->split_input[0]);
-	printf("Struct element 0: heredoc_delim: %s\n", cmds[0]->heredoc_delim);
-	printf("Struct element 0: heredoc: %d\n", cmds[0]->heredoc);
-	parse_input_segments(sh, *cmds);
-	printf("Struct element 0: heredoc_delim: %s\n", cmds[0]->heredoc_content);
-	printf("Struct element 1: heredoc: %d\n", cmds[0]->heredoc);
+	int i = 0;
+	while (sh->cmds[i])
+	{
+		printf("Struct %d: segment: %s\n", i, sh->cmds[i]->segment);
+		i++;
+	}
+	//printf("Struct 2: segment: %s\n", sh->cmds[2]->segment);
+	//printf("Struct 0: heredoc_delim: %s\n", sh->cmds[0]->heredoc_delim);
+	//printf("Struct 0: heredoc: %d\n", sh->cmds[0]->heredoc);
+	//parse_input_segments(sh, *cmds);
+	printf("Struct 0: heredoc_delim: %s\n", sh->cmds[0]->heredoc_content);
+	printf("Struct 0: heredoc: %d\n", sh->cmds[0]->heredoc);
 	//if (parse_input(mini, *cmds))
 	//	return (1);
 	return (0);
@@ -43,19 +49,18 @@ static int user_prompt()
 {
 	char		*input;
 	t_shell		sh;
-	t_command	*cmds;
 
 	//init_shell(&sh, envp);
 	//init_sig();
 	while (1)
 	{
+		initialize_struct(&sh);
 		input = readline("minishell> ");
 		if (input == NULL)
 			break ;
 		if (input && *input)
-			add_history(input);
-		initialize_struct(&sh);
-		if (parse_and_validate_input(input, &sh, &cmds))
+			add_history(input);	
+		if (parse_and_validate_input(input, &sh))
 			return (1);
 	}
 	return (0);
