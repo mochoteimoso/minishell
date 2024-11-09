5b204e6fd1f7bf24362d29d518d18b1c8a6aa7f7
Heidi E <henbuska@student.hive.fi>
2024-11-09 18:37:50 +0200

    Change from cmds, index to t_command cmd

--- sources/parser/handle_redirections.c
+++ sources/parser/handle_redirections.c
@@ -1,28 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   redirections.c                                     :+:      :+:    :+:   */
+/*   handle_redirections.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 19:05:32 by henbuska          #+#    #+#             */
-/*   Updated: 2024/11/08 15:58:09 by henbuska         ###   ########.fr       */
+/*   Updated: 2024/11/09 18:08:12 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-bool	is_redirection(char *str, int index);
-int		handle_redirect_in(char *str, int *i, t_shell *sh, int index);
-int		handle_redirect_out(char *str, int *i, t_shell *sh, int index);
-int		handle_heredoc(char *str, int *i, t_shell *sh, int index);
-int		handle_append(char *str, int *i, t_shell *sh, int index);
+bool	is_redirection(t_cmd *cmd, int i);
+int		handle_redirect_in(t_cmd *cmd, int *i);
+int		handle_redirect_out(t_cmd *cmd, int *i);
+int		handle_heredoc(t_cmd *cmd, int *i);
+int		handle_append(t_cmd *cmd, int *i);
 
 // Checks if the input contains a redirection symbol that is not within quotes
 
-bool	is_redirection(char *str, int i)
+bool	is_redirection(t_cmd *cmd, int i)
 {
-	if ((str[i] == '>' || str[i] == '<') && !is_in_quotes(str, i))
+	if ((cmd->segment[i] == '>' || cmd->segment[i] == '<') && 
+		!is_in_quotes(cmd->segment, i))
 		return (true);
 	else
 		return (false);
@@ -30,63 +31,64 @@ bool	is_redirection(char *str, int i)
 
 // Handles < redirection, finds the filename and copies data to the redir linked list  
 
-int	handle_redirect_in(char *str, int *i, t_shell *sh, int index)
+int	handle_redirect_in(t_cmd *cmd, int *i)
 {
 	char	*filename_start;
 	int		filename_length;
 	
 	filename_length = 0;
 	(*i)++;
-	while (str[*i] && ft_isspace(str[*i]))
+	while (cmd->segment[*i] && ft_isspace(cmd->segment[*i]))
 		(*i)++;
-	filename_start = &str[*i];
-	while (str[*i] && str[*i] != ' ' && str[*i] != '|' && str[*i] != '<' &&
-		str[*i] != '>' && str[*i] != '$')
+	filename_start = &cmd->segment[*i] ;
+	while (cmd->segment[*i] && cmd->segment[*i] != ' ' && cmd->segment[*i] != '|' &&
+		cmd->segment[*i] != '<' && cmd->segment[*i] != '>' && cmd->segment[*i] != '$')
 	{
 		filename_length++;
 		(*i)++;
 	}
-	sh->cmds[index]->redirect_in = ft_strndup(filename_start, filename_length);
-	if (!sh->cmds[index]->redirect_in)
+	list_redir();
+	mini->cmds[index]->redirect_in = ft_strndup(filename_start, filename_length);
+	if (!mini->cmds[index]->redirect_in)
 	{
 		printf("Failed to allocate memory for filename\n");
 		return (1);
 	}
-	sh->cmds[index]->redirect_type = REDIRECT_IN;
+	mini->cmds[index]->redirect_type = REDIRECT_IN;
 	return (0);
 }
 
 // Handles > redirection, finds the filename and copies data to the redir linked list  
 
-int	handle_redirect_out(char *str, int *i, t_shell *sh, int index)
+int	handle_redirect_out(t_cmd *cmd, int *i)
 {
 	char	*filename_start;
 	int		filename_length;
 	
 	filename_length = 0;
 	(*i)++;
-	while (str[*i] && ft_isspace(str[*i]))
+	while (cmd->segment[*i] && ft_isspace(cmd->segment[*i]))
 		(*i)++;
-	filename_start = &str[*i];
-	while (str[*i] && str[*i] != ' ' && str[*i] != '|' && str[*i] != '<' &&
-		str[*i] != '>' && str[*i] != '$')
+	filename_start = &cmd->segment[*i];
+	while (cmd->segment[*i] && cmd->segment[*i] != ' ' && cmd->segment[*i] != '|' &&
+		cmd->segment[*i] != '<' && cmd->segment[*i] != '>' && cmd->segment[*i]!= '$')
 	{
 		filename_length++;
 		(*i)++;
 	}
-	sh->cmds[index]->redirect_out = ft_strndup(filename_start, filename_length);
-	if (!sh->cmds[index]->redirect_out)
+	mini->cmds[index]->redirect_out = ft_strndup(filename_start, filename_length);
+	if (!mini->cmds[index]->redirect_out)
 	{
 		printf("Failed to allocate memory for filename\n");
 		return (1);
 	}
-	sh->cmds[index]->redirect_type = REDIRECT_OUT;
+	mini->cmds[index]->redirect_type = REDIRECT_OUT;
 	return (0);
 }
 
 // Handles heredoc, finds the delimiter and copies data to the redir linked list  
 
-int	handle_heredoc(char *str, int *i, t_shell *sh, int index)
+int	handle_heredoc(t_cmd *cmd, int *i)
 {
 	char	*delimiter_start;
 	int		delimiter_length;
@@ -94,30 +96,30 @@ int	handle_heredoc(char *str, int *i, t_shell *sh, int index)
 	delimiter_length = 0;
 	(*i)++;
 	(*i)++;
-	while (str[*i] && ft_isspace(str[*i]))
+	while (cmd->segment[*i]&& ft_isspace(cmd->segment[*i]))
 		(*i)++;
-	delimiter_start = &str[*i];
-	while (str[*i] && str[*i] != ' ' && str[*i] != '|' && str[*i] != '<' &&
-		str[*i] != '>' && str[*i] != '$')
+	delimiter_start = &cmd->segment[*i];
+	while (cmd->segment[*i] && cmd->segment[*i] != ' ' && cmd->segment[*i] != '|' &&
+		cmd->segment[*i]!= '<' && cmd->segment[*i]!= '>' && cmd->segment[*i] != '$')
 	{
 		delimiter_length++;
 		(*i)++;
 	}
-	sh->cmds[index]->heredoc_delim = ft_strndup(delimiter_start, delimiter_length);
-	if (!sh->cmds[index]->heredoc_delim)
+	mini->cmds[index]->heredoc_delim = ft_strndup(delimiter_start, delimiter_length);
+	if (!mini->cmds[index]->heredoc_delim)
 	{
 		printf("Failed to allocate memory for heredoc delimiter\n");
 		return (1);
 	}
 	//printf("heredoc_delim after copy: %s\n", sh->cmds[index]->heredoc_delim);
-	sh->cmds[index]->redirect_type = HEREDOC;
-	sh->cmds[index]->heredoc = true;
+	mini->cmds[index]->redirect_type = HEREDOC;
+	mini->cmds[index]->heredoc = true;
 	return (0);
 }
 
 // Handles append redirection, finds the filename and copies data to the redir linked list  
 
-int	handle_append(char *str, int *i, t_shell *sh, int index)
+int	handle_append(t_cmd *cmd, int *i)
 {
 	char	*filename_start;
 	int		filename_length;
@@ -125,21 +127,21 @@ int	handle_append(char *str, int *i, t_shell *sh, int index)
 	filename_length = 0;
 	(*i)++;
 	(*i)++;
-	while (str[*i] && ft_isspace(str[*i]))
+	while (cmd->segment[*i]&& ft_isspace(cmd->segment[*i]))
 		(*i)++;
-	filename_start = &str[*i];
-	while (str[*i] && str[*i] != ' ' && str[*i] != '|' && str[*i] != '<' &&
-		str[*i] != '>' && str[*i] != '$')
+	filename_start = &cmd->segment[*i];
+	while (cmd->segment[*i] && cmd->segment[*i] != ' ' && cmd->segment[*i] != '|' &&
+		cmd->segment[*i]!= '<' && cmd->segment[*i]!= '>' && cmd->segment[*i] != '$')
 	{
 		filename_length++;
 		(*i)++;
 	}
-	sh->cmds[index]->append = ft_strndup(filename_start, filename_length);
-	if (!sh->cmds[index]->append)
+	mini->cmds[index]->append = ft_strndup(filename_start, filename_length);
+	if (!mini->cmds[index]->append)
 	{
 		printf("Failed to allocate memory for filename\n");
 		return (1);
 	}
-	sh->cmds[index]->redirect_type = APPEND;
+	mini->cmds[index]->redirect_type = APPEND;
 	return (0);
 }
