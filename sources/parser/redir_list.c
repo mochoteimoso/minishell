4e43cf60a7c3b43789cf0d87eed018dfa86367a9
Heidi E <henbuska@student.hive.fi>
2024-11-11 13:39:10 +0200

    Work in progress for linked list functions

--- sources/parser/redir_list.c
+++ sources/parser/redir_list.c
@@ -6,13 +6,13 @@
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 11:42:04 by nzharkev          #+#    #+#             */
-/*   Updated: 2024/11/09 18:26:07 by henbuska         ###   ########.fr       */
+/*   Updated: 2024/11/11 13:26:18 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static void	redir_lstadd_back(t_redir **lst, t_redir *new)
+void	redir_lstadd_back(t_redir **lst, t_redir *new)
 {
 	t_redir	*temp;
 
@@ -29,7 +29,7 @@ static void	redir_lstadd_back(t_redir **lst, t_redir *new)
 	temp->next = new;
 }
 
-static t_redir	*redir_add_node(void)
+t_redir	*redir_add_node(void)
 {
 	t_redir	*node;
 
@@ -38,19 +38,15 @@ static t_redir	*redir_add_node(void)
 		return (NULL);
 	node->delimiter = NULL;
 	node->file = NULL;
-	node->type = NULL;
+	node->type = 0;
 	node->next = NULL;
-	node->node_ind = NULL;
 	return (node);
 }
 
 t_redir	*list_redir(void)
 {
-	t_redir	*ll;
-	t_redir	*new;
+	t_redir *new;
 
-	ll = NULL;
 	new = redir_add_node();
-	redir_lstadd_back(&ll, new);
-	return (ll);
+	return (new);
 }
