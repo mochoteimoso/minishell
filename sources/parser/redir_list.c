2674fdb1958792a491eec1ffdef0f4a8426a252f
Heidi E <henbuska@student.hive.fi>
2024-11-11 18:42:19 +0200

    Add redir_update_tail function

--- sources/parser/redir_list.c
+++ sources/parser/redir_list.c
@@ -6,7 +6,7 @@
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 11:42:04 by nzharkev          #+#    #+#             */
-/*   Updated: 2024/11/11 13:26:18 by henbuska         ###   ########.fr       */
+/*   Updated: 2024/11/11 15:04:55 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,24 @@ t_redir	*redir_add_node(void)
 t_redir	*list_redir(void)
 {
 	t_redir *new;
-
+	t_redir *ll;
+	
+	ll = NULL;
 	new = redir_add_node();
-	return (new);
+	redir_lstadd_back(&ll, new);
+	return (ll);
+}
+
+void redir_update_tail(t_cmd *cmd)
+{
+	t_redir *new;
+	t_redir *temp;
+	
+	temp = NULL;
+	new = redir_add_node();
+	redir_lstadd_back(&cmd->redir_head, new);
+	temp = cmd->redir_head;
+	while (temp->next)
+		temp = temp->next;
+	cmd->redir_tail = temp;
 }
