a15bc612a0feaeb59b6d44a25f68f955bc5f5155
Heidi E <henbuska@student.hive.fi>
2024-11-13 16:10:29 +0200

    Add new file and functions for resolving input and output fd

--- sources/redirection/redirector.c
+++ sources/redirection/redirector.c
@@ -6,9 +6,185 @@
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 13:10:23 by henbuska          #+#    #+#             */
-/*   Updated: 2024/11/12 13:10:34 by henbuska         ###   ########.fr       */
+/*   Updated: 2024/11/13 15:56:59 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+int	resolve_fd(t_cmd *cmd);
+int resolve_input_fd(t_cmd *cmd);
+int resolve_output_fd(t_cmd *cmd);
+int	open_input_file(char *input_file);
+int	open_output_file(char *output_file);
+int	open_append_file(char *output_file);
+int open_heredoc(char *delimiter);
+
+// Figures out fds for input and output
+
+int	resolve_fd(t_cmd *cmd)
+{
+	int	fd_in;
+	int	fd_out;
+
+	fd_in = resolve_input_fd(cmd);
+	if (fd_in == -1)
+		return (EXIT_FAILURE);
+	cmd->fd_in = fd_in;
+	fd_out = resolve_output_fd(cmd);
+	if (fd_out == -1)
+	{	close(fd_in);
+		return (EXIT_FAILURE);
+	}
+	cmd->fd_out = fd_out;
+	return (0);
+}
+
+// Gets fd for input file
+
+int resolve_input_fd(t_cmd *cmd)
+{
+	int		fd_in;
+	t_redir	*temp;
+
+	temp = cmd->redir_head;
+	while (temp && (temp->type == 1 || temp->type == 3))
+	{
+		if (fd_in != -1)
+			close(fd_in);
+		if (temp->type == 1)
+			fd_in = open_input_file(temp->file);
+		else if (temp->type == 3)
+			fd_in = open_heredoc(temp->delimiter);
+		if (fd_in == -1)
+			return (-1);
+		temp = temp->next;
+	}
+	return (fd_in);
+}
+
+// Gets fd for output file
+
+int resolve_output_fd(t_cmd *cmd)
+{
+	int		fd_out;
+	t_redir	*temp;
+
+	temp = cmd->redir_head;
+	while (temp && (temp->type == 2 || temp->type == 4))
+	{
+		if (fd_out != -1)
+			close(fd_out);
+		if (temp->type == 2)
+			fd_out = open_output_file(temp->file);
+		else if (temp->type == 4)
+			fd_out = open_append_file(temp->file);
+		if (fd_out == -1)
+			return (-1);
+		temp = temp->next;
+	}
+	return (fd_out);
+}
+
+// Tries to open input file and prints correct error in case of failure
+
+int	open_input_file(char *input_file)
+{
+	int	fd_in;
+
+	fd_in = open(input_file, O_RDONLY);
+	if (fd_in == -1)
+	{
+		if (access(input_file, F_OK) != 0)
+		{
+			ft_putstr_fd(input_file, 2);
+			ft_putstr_fd(": No such file or directory\n", 2);
+		}
+		else
+		{
+			ft_putstr_fd(input_file, 2);
+			ft_putstr_fd(": Permission denied\n", 2);
+		}
+		return(-1);
+	}
+	return (fd_in);
+}
+
+// Tries to open output file and prints correct error in case of failure
+
+int	open_output_file(char *output_file)
+{
+	int	fd_out;
+
+	fd_out = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd_out == -1)
+	{
+		if (access(output_file, F_OK))
+		{
+			ft_putstr_fd(output_file, 2);
+			ft_putstr_fd(": No such file or directory\n", 2);
+		}
+		else
+		{
+			ft_putstr_fd(output_file, 2);
+			ft_putstr_fd(": Permission denied\n", 2);
+		}
+		return(-1);
+	}
+	return (fd_out);
+}
+
+// Tries to open output append file and prints correct error in case of failure
+
+int	open_append_file(char *output_file)
+{
+	int	fd_out;
+
+	fd_out = open(output_file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	if (fd_out == -1)
+	{
+		if (access(output_file, F_OK))
+		{
+			ft_putstr_fd(output_file, 2);
+			ft_putstr_fd(": No such file or directory\n", 2);
+		}
+		else
+		{
+			ft_putstr_fd(output_file, 2);
+			ft_putstr_fd(": Permission denied\n", 2);
+		}
+		return(-1);
+	}
+	return (fd_out);
+}
+
+// Opens heredoc
+
+int	open_heredoc(char *delimiter) 
+{
+	int		heredoc_pipe_fd[2];
+	char	*line;
+
+	if (pipe(heredoc_pipe_fd) == -1)
+	{
+		perror("pipe error");
+		return (-1);
+	}
+	while (1)
+	{
+		line = readline("heredoc> ");
+		if (!line || ft_strcmp(line, delimiter) == 0)
+		{
+			free(line);
+			break ;
+		}
+		write(heredoc_pipe_fd[1], line, ft_strlen(line));
+		write(heredoc_pipe_fd[1], "\n", 1);
+		free(line);
+	}
+	close(heredoc_pipe_fd[1]);
+	return (heredoc_pipe_fd[0]);
+}
+
+
 
