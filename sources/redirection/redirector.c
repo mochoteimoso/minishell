39a40a5f03c555c55067b8364b9f809565c53add
Heidi E <henbuska@student.hive.fi>
2024-11-14 16:22:48 +0200

    Edit resolve_fd functions to work when there is either input or output redirection, not both

--- sources/redirection/redirector.c
+++ sources/redirection/redirector.c
@@ -6,7 +6,7 @@
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 13:10:23 by henbuska          #+#    #+#             */
-/*   Updated: 2024/11/13 15:56:59 by henbuska         ###   ########.fr       */
+/*   Updated: 2024/11/14 13:21:51 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,33 +26,94 @@ int	resolve_fd(t_cmd *cmd)
 {
 	int	fd_in;
 	int	fd_out;
-
+	
+	fd_in = -1;
+	fd_out = -1;	
 	fd_in = resolve_input_fd(cmd);
-	if (fd_in == -1)
-		return (EXIT_FAILURE);
-	cmd->fd_in = fd_in;
+	//if (fd_in == -1 && cmd->redir_head)
+	//	return (EXIT_FAILURE);
+	if (fd_in != -1)
+		cmd->fd_in = fd_in;
+	else
+		cmd->fd_in = STDIN_FILENO;
 	fd_out = resolve_output_fd(cmd);
-	if (fd_out == -1)
-	{	close(fd_in);
-		return (EXIT_FAILURE);
-	}
-	cmd->fd_out = fd_out;
+	//if (fd_out == -1 && cmd->redir_head)
+	//{disp
+	//	if (fd_in != -1)
+	//		close(fd_in);
+	//	return (EXIT_FAILURE);
+	//}
+	if (fd_out != -1)
+		cmd->fd_out = fd_out;
+	else
+		cmd->fd_out = STDOUT_FILENO;
 	return (0);
 }
 
-// Gets fd for input file
+int	resolve_input_fd(t_cmd *cmd)
+{
+	int		fd_in;
+	t_redir	*temp;
 
+	fd_in = -1;
+	temp = cmd->redir_head;
+	while (temp)
+	{
+		if (temp->type == 0 || temp->type == 3)
+		{
+			if (fd_in != -1)
+				close(fd_in);
+			if (temp->type == 0)
+				fd_in = open_input_file(temp->file);
+			else if (temp->type == 3)
+				fd_in = open_heredoc(temp->delimiter);
+			if (fd_in == -1)
+				return (-1);
+		}
+		temp = temp->next;
+	}
+	return (fd_in);
+}
+
+int	resolve_output_fd(t_cmd *cmd)
+{
+	int		fd_out;
+	t_redir	*temp;
+
+	fd_out = -1;
+	temp = cmd->redir_head;
+	while (temp)
+	{
+		if (temp->type == 1 || temp->type == 2)
+		{
+			if (fd_out != -1)
+				close(fd_out);
+			if (temp->type == 1)
+				fd_out = open_output_file(temp->file);
+			else if (temp->type == 2)
+				fd_out = open_append_file(temp->file);
+			if (fd_out == -1)
+				return (-1);
+		}
+		temp = temp->next;
+	}
+	return (fd_out);
+}
+
+// Gets fd for input file
+/*
 int resolve_input_fd(t_cmd *cmd)
 {
 	int		fd_in;
 	t_redir	*temp;
 
+	fd_in = -1;
 	temp = cmd->redir_head;
-	while (temp && (temp->type == 1 || temp->type == 3))
+	while (temp && (temp->type == 0 || temp->type == 3))
 	{
 		if (fd_in != -1)
 			close(fd_in);
-		if (temp->type == 1)
+		if (temp->type == 0)
 			fd_in = open_input_file(temp->file);
 		else if (temp->type == 3)
 			fd_in = open_heredoc(temp->delimiter);
@@ -70,21 +131,22 @@ int resolve_output_fd(t_cmd *cmd)
 	int		fd_out;
 	t_redir	*temp;
 
+	fd_out = -1;
 	temp = cmd->redir_head;
-	while (temp && (temp->type == 2 || temp->type == 4))
+	while (temp && (temp->type == 1 || temp->type == 2))
 	{
 		if (fd_out != -1)
 			close(fd_out);
-		if (temp->type == 2)
+		if (temp->type == 1)
 			fd_out = open_output_file(temp->file);
-		else if (temp->type == 4)
+		else if (temp->type == 2)
 			fd_out = open_append_file(temp->file);
 		if (fd_out == -1)
 			return (-1);
 		temp = temp->next;
 	}
 	return (fd_out);
-}
+} */
 
 // Tries to open input file and prints correct error in case of failure
 
