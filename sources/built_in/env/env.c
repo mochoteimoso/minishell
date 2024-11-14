1204cd87f846202c7ed11f37cd8e6ccc5a9a0973
Heidi E <henbuska@student.hive.fi>
2024-11-14 16:19:19 +0200

    Add env_to_array function

--- sources/built_in/env/env.c
+++ sources/built_in/env/env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nzharkev <nzharkev@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 13:24:29 by nzharkev          #+#    #+#             */
-/*   Updated: 2024/11/07 13:01:07 by nzharkev         ###   ########.fr       */
+/*   Updated: 2024/11/14 14:19:58 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,31 @@ int	built_env(t_shell *mini)
 		temp = temp->next;
 	}
 	return (0);
+}
+
+char	**env_to_array(t_env *env)
+{
+	t_env	*temp;
+	char	**array;
+	int		i;
+	int		len;
+
+	temp = env;
+	len = ft_lstsize((t_list *)temp);
+	temp = env;
+	i = 0;
+	array = (char **)malloc(sizeof(char) * len + 1);
+	while (temp->next)
+	{
+		array[i] = ft_strdup("");
+		array[i] = ft_strjoin(array[i], temp->name);
+		array[i] = ft_strjoin(array[i], "=");
+		array[i] = ft_strjoin(array[i], temp->value);
+		temp = temp->next;
+		printf("%s\n", array[i]);
+		i++;
+	}
+	// add error handling
+	array[i] = NULL;
+	return (array);
 }
