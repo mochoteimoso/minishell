6104ddc5475b9e9b68ae56d3f1de0e99fa403903
Heidi E <henbuska@student.hive.fi>
2024-11-16 18:34:18 +0200

    Add redirll_head_tail function

--- sources/redirection/redir_ll.c
+++ sources/redirection/redir_ll.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   redir_list.c                                       :+:      :+:    :+:   */
+/*   redir_ll.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 11:42:04 by nzharkev          #+#    #+#             */
-/*   Updated: 2024/11/11 15:04:55 by henbuska         ###   ########.fr       */
+/*   Updated: 2024/11/16 15:27:50 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,4 +66,28 @@ void redir_update_tail(t_cmd *cmd)
 	while (temp->next)
 		temp = temp->next;
 	cmd->redir_tail = temp;
+}
+
+int	redirll_head_tail(t_cmd *cmd)
+{
+	if (!cmd->redir_head)
+	{
+		cmd->redir_head = list_redir();
+		if (!cmd->redir_head)
+		{
+			printf("Failed to initialize redirection list\n");
+			return (-1);
+		}
+		cmd->redir_tail = cmd->redir_head;
+	}
+	else
+	{
+		redir_update_tail(cmd);
+		if (!cmd->redir_tail)
+		{
+			printf("Failed to allocate memory for new redirection node\n");
+			return (-1);
+		}
+	}
+	return(0);
 }
