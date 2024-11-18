114f2d252a31972d2216d2103370013bb58f41ca
Heidi E <henbuska@student.hive.fi>
2024-11-18 15:31:52 +0200

    Add redirection check to cmd_args loop

--- sources/parser/handle_cmd_args.c
+++ sources/parser/handle_cmd_args.c
@@ -16,40 +16,7 @@
 int	handle_cmd_args(t_cmd *cmd, int i);
 int	count_args(t_cmd *cmd, int i);
 
-// Retrieves command arguments from string and copies them to struct
-
-int	handle_cmd_args(t_cmd *cmd, int i)
-{
-	int		arg_len;
-	char	*arg_start;
-	int		args_count;
-	int		arg_index;
-	
-	arg_index = 0;
-	args_count = count_args(cmd, i);
-	cmd->args = ft_calloc(args_count + 2, sizeof(char *));
-	if (!cmd->args)
-		return (-1);
-	cmd->args[arg_index] = ft_strdup(cmd->command);
-	arg_index++;
-	i = skip_whitespace(cmd->segment, i);
-	while (cmd->segment[i] && arg_index < args_count + 1)
-	{
-		if (cmd->segment[i] == '\'' || cmd->segment[i] == '"')
-			i = arg_in_quotes(cmd->segment, i, &arg_start, &arg_len);
-		else
-			i = arg_no_quotes(cmd, i, &arg_start, &arg_len);
-		if (append_to_array(cmd, arg_start, arg_len, &arg_index) == -1)
-			return (-1);
-		i = skip_whitespace(cmd->segment, i);
-	}
-	cmd->args[arg_index] = NULL;
-	//printf("index after handle_args: %d\n", i);
-	return (i);
-}
-
 // Counts how many command arguments the string contains
-
 int	count_args(t_cmd *cmd, int i)
 {
 	int	args_count;
@@ -62,15 +29,44 @@ int	count_args(t_cmd *cmd, int i)
 		if (cmd->segment[i] && !is_redirection(cmd, i))
 		{
 			args_count++;
-			while (cmd->segment[i]&& !ft_isspace(cmd->segment[i]) && 
+			while (cmd->segment[i]&& !ft_isspace(cmd->segment[i]) &&
 			!is_redirection(cmd, i))
 				i++;
 		}
 		else if (is_redirection(cmd, i))
 			break ;
 		while (cmd->segment[i]&& ft_isspace(cmd->segment[i]))
+
 			i++;
 	}
 	//printf("Argument count: %d\n", args_count);
 	return (args_count);
+}
+
+int	handle_cmd_args(t_cmd *cmd, int i)
+{
+	int		arg_len;
+	int		args_count;
+	int		arg_index;
+	char	*arg_start;
+
+	args_count = count_args(cmd, i);
+	cmd->args = ft_calloc(args_count + 2, sizeof(char *));
+	if (!cmd->args)
+		return (-1);
+	arg_index = 1;
+	cmd->args[0] = ft_strdup(cmd->command);
+	i = skip_whitespace(cmd->segment, i);
+	while (cmd->segment[i] && arg_index < args_count + 1 && !is_redirection(cmd, i))
+	{
+		if (cmd->segment[i] == '\'' || cmd->segment[i] == '"')
+			i = arg_in_quotes(cmd->segment, i, &arg_start, &arg_len);
+		else
+			i = arg_no_quotes(cmd, i, &arg_start, &arg_len);
+		if (append_to_array(cmd, arg_start, arg_len, &arg_index) == -1)
+			return (-1);
+		i = skip_whitespace(cmd->segment, i);
+	}
+	cmd->args[arg_index] = NULL;
+	return (i);
 }
