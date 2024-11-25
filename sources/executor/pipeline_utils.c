ad81dfe130433ee68ee89eafdb44a37e0f798f84
Heidi E <henbuska@student.hive.fi>
2024-11-25 19:38:31 +0200

    Move some functions to different file

--- sources/executor/pipeline_utils.c
+++ sources/executor/pipeline_utils.c
@@ -5,116 +5,54 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/15 09:58:47 by henbuska          #+#    #+#             */
-/*   Updated: 2024/11/25 13:35:12 by henbuska         ###   ########.fr       */
+/*   Created: 2024/11/25 17:01:57 by henbuska          #+#    #+#             */
+/*   Updated: 2024/11/25 18:17:26 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int		dup_input(t_shell *mini, t_cmd *cmd, int i);
-int		dup_output(t_cmd *cmd, int pipe_fd[2], int count, int i);
-int		dup2_and_close(int old_fd, int new_fd);
-void	close_pipe_fds(int pipe_fd[2]);
-void	close_pipes(t_shell *mini, int pipe_fd[2]);
+void	close_fds_and_pipes(t_shell *mini, t_cmd *cmd, int pipe_fd[2], int i);
+void	wait_children(t_shell *mini);
 
-// Duplicates input from fd or pipe_fd
+// Closes current command's fds, previous pipe and pipe_fds when needed
 
-int	dup_input(t_shell *mini, t_cmd *cmd, int i)
+void	close_fds_and_pipes(t_shell *mini, t_cmd *cmd, int pipe_fd[2], int i)
 {
 	if (cmd->fd_in != STDIN_FILENO)
-	{
-		printf("Redirecting input: cmd->fd_in = %d\n", cmd->fd_in);
-		if (dup2_and_close(cmd->fd_in, STDIN_FILENO))
-		{
-			perror("dup2 for input redirection");
-			return (1);
-		}
-		if (mini->prev_pipe != -1)
-			close(mini->prev_pipe);
-		printf("Input redirection handled for cmd[%d].\n", i);
-	}
-	else if (i > 0) // Middle or last command
-	{
-		printf("Using pipe for input: pipe_fd[0] = %d\n", mini->prev_pipe);
-		if (dup2_and_close(mini->prev_pipe, STDIN_FILENO) == -1)
-		{
-			perror("dup2 for pipe input");
-			return (1);
-		}
-		printf("Pipe input handled for cmd[%d].\n", i);
-	}
-	return (0);
-}
-
-// Duplicates output to fd or pipe_fd
-
-int	dup_output(t_cmd *cmd, int pipe_fd[2], int count, int i)
-{
+		close(cmd->fd_in);
 	if (cmd->fd_out != STDOUT_FILENO)
+		close(cmd->fd_out);
+	if (i > 0)
 	{
-		printf("Redirecting output: cmd->fd_out = %d\n", cmd->fd_out);
-		if (dup2_and_close(cmd->fd_out, STDOUT_FILENO))
-		{
-			perror("dup2 for output redirection");
-			return (1);
-		}
-		if (i < count - 1)
-		{
-			close(pipe_fd[0]);
-			close(pipe_fd[1]);
-		}
+		close(mini->prev_pipe);
+		//mini->prev_pipe = -1;
 	}
-	else if (i < count - 1)
+	
+	if (i < mini->cmd_count - 1)
 	{
-		printf("Using pipe for output: pipe_fd[1] = %d\n", pipe_fd[1]);
-		if (dup2_and_close(pipe_fd[1], STDOUT_FILENO))
-		{
-			perror("dup2 for restoring stdout");
-			return (1);
-		}
-		//close(cmd->fd_out);
-		close(pipe_fd[0]);
+		mini->prev_pipe = pipe_fd[0];  // Store the current pipe's read end for the next command
+		close(pipe_fd[1]);
 	}
-	return (0);
 }
 
-// Helper function to duplicate and close fd
+// Waits for all child prcocesses to complete and catches their exit status
 
-int	dup2_and_close(int old_fd, int new_fd)
+void	wait_children(t_shell *mini)
 {
-	if (old_fd < 0)
+	int	i;
+	int	status;
+	
+	i = 0;
+	status = 0;
+	while (i < mini->cmd_count)
 	{
-		ft_putendl_fd("Invalid file descriptor", 2);
-		return (1);
-	}
-	if (dup2(old_fd, new_fd) == -1)
-	{
-		close(old_fd);
-		return (1);
-	}
-	close(old_fd);
-	return (0);
-}
-
-// Close all pipes in the array
-
-void	close_pipe_fds(int pipe_fd[2])
-{
-	close(pipe_fd[0]);
-	close(pipe_fd[1]);
-}
-
-void	close_pipes(t_shell *mini, int pipe_fd[2])
-{
-	if (mini->prev_pipe != -1)
-	{
-		close(mini->prev_pipe); // Close the previous read end
-		mini->prev_pipe = -1;  // Reset it
-	}
-	if (pipe_fd[1] != -1)
-	{
-		close(pipe_fd[1]); // Close the current write end
-		pipe_fd[1] = -1;   // Reset it
+		waitpid(mini->pids[i], &status, 0);
+		if (WIFEXITED(status))
+			mini->exit_stat = WEXITSTATUS(status);
+		else if (WIFSIGNALED(status))
+			printf("Child %d terminated by signal %d\n", mini->pids[i], WTERMSIG(status));
+		i++;
 	}
 }
+
