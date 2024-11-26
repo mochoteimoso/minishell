7ab46d2cfa0ee752f024d8fc3031da57606c058f
Heidi E <henbuska@student.hive.fi>
2024-11-26 19:37:28 +0200

    Add modified version of dup2_and_close for main process

--- sources/executor/pipeline_utils.c
+++ sources/executor/pipeline_utils.c
@@ -6,22 +6,44 @@
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 17:01:57 by henbuska          #+#    #+#             */
-/*   Updated: 2024/11/26 10:08:15 by henbuska         ###   ########.fr       */
+/*   Updated: 2024/11/26 19:00:42 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+int		dup2_and_close_in_main(t_shell *mini, int old_fd, int new_fd);
 void	close_fds_and_pipes(t_shell *mini, t_cmd *cmd, int pipe_fd[2], int i);
 void	wait_children(t_shell *mini);
+
+// Duplicates fds in main process and resets fds to STDIN/STDOUT when needed
+// only difference is that this resets fds in case of error here to save space
+
+int	dup2_and_close_in_main(t_shell *mini, int old_fd, int new_fd)
+{
+	if (old_fd < 0)
+	{
+		ft_putendl_fd("Invalid file descriptor", 2);
+		reset_fds(mini);
+		return (1);
+	}
+	if (dup2(old_fd, new_fd) == -1)
+	{
+		close(old_fd);
+		reset_fds(mini);
+		return (1);
+	}
+	close(old_fd);
+	return (0);
+}
 
 // Closes current command's fds, previous pipe and pipe_fds when needed
 
 void	close_fds_and_pipes(t_shell *mini, t_cmd *cmd, int pipe_fd[2], int i)
 {
-	if (cmd->fd_in != STDIN_FILENO)
+	if (cmd->fd_in != STDIN_FILENO && cmd->fd_in != -1)
 		close(cmd->fd_in);
-	if (cmd->fd_out != STDOUT_FILENO)
+	if (cmd->fd_out != STDOUT_FILENO && cmd->fd_out != -1)
 		close(cmd->fd_out);
 	if (i > 0)
 	{
