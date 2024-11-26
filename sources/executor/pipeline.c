a3d7ac44ad13c1b63e4b082e32e1b255e42463d8
Heidi E <henbuska@student.hive.fi>
2024-11-26 12:57:31 +0200

    Moved operator to beginning of line

--- sources/executor/pipeline.c
+++ sources/executor/pipeline.c
@@ -6,7 +6,7 @@
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 13:28:23 by henbuska          #+#    #+#             */
-/*   Updated: 2024/11/25 18:58:11 by henbuska         ###   ########.fr       */
+/*   Updated: 2024/11/26 10:04:51 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ static int	pipe_and_fork(t_shell *mini, int pipe_fd[2]);
 // Sets up pipeline and forks child processes when needed
 // Allocates memory for an array of pids to store IDs of child processes
 
-int	execute_pipeline(t_shell *mini) 
+int	execute_pipeline(t_shell *mini)
 {
 	int	pipe_fd[2];
-	
+
 	if (mini->cmd_count == 1 && is_this_built(mini->cmds[0]->command))
 	{
 		if (handle_single_builtin_cmd(mini))
@@ -32,7 +32,7 @@ int	execute_pipeline(t_shell *mini)
 	mini->pids = ft_calloc(mini->cmd_count, sizeof(pid_t));
 	if (!mini->pids)
 	{
-		clean_cmds(mini->cmds);   // create a function that also sets pointer to null!
+		clean_cmds(mini->cmds); // create a function that also sets pointer to null!
 		return (1);
 	}
 	if (pipe_and_fork(mini, pipe_fd))
@@ -90,7 +90,8 @@ static int	pipe_and_fork(t_shell *mini, int pipe_fd[2])
 	while (i < mini->cmd_count)
 	{
 		cmd = mini->cmds[i];
-		if (mini->cmd_count > 1 && i < mini->cmd_count - 1 && pipe(pipe_fd) == -1)
+		if (mini->cmd_count > 1 && i < mini->cmd_count - 1
+			&& pipe(pipe_fd) == -1)
 		{
 			perror("pipe");
 			return (1);
@@ -102,4 +103,3 @@ static int	pipe_and_fork(t_shell *mini, int pipe_fd[2])
 	}
 	return (0);
 }
-
