c14919eb2937582ab5bcc260baf69820fcc676ed
Heidi E <henbuska@student.hive.fi>
2024-11-27 16:08:13 +0200

    Add conditions to dups

--- sources/executor/fd_handler.c
+++ sources/executor/fd_handler.c
@@ -6,7 +6,7 @@
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 15:18:10 by henbuska          #+#    #+#             */
-/*   Updated: 2024/11/26 17:04:17 by henbuska         ###   ########.fr       */
+/*   Updated: 2024/11/27 10:42:20 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,25 @@
 
 int	save_fds(t_shell *mini)
 {
-	mini->stdin_saved = dup(STDIN_FILENO);
-	if (mini->stdin_saved == -1)
+	if (mini->cmds[0]->fd_in != STDIN_FILENO)
 	{
-		mini->exit_stat = 1;
-		return (perror("Failed to save STDIN"), 1);
+		mini->stdin_saved = dup(STDIN_FILENO);
+		if (mini->stdin_saved == -1)
+		{
+			mini->exit_stat = 1;
+			return (perror("Failed to save STDIN"), 1);
+		}
 	}
-	mini->stdout_saved = dup(STDOUT_FILENO);
-	if (mini->stdout_saved == -1)
+	if (mini->cmds[0]->fd_out != STDOUT_FILENO)
 	{
-		close(mini->stdin_saved);
-		mini->exit_stat = 1;
-		return (perror("Failed to save STDOUT"), 1);
+		mini->stdout_saved = dup(STDOUT_FILENO);
+		if (mini->stdout_saved == -1)
+		{
+			if (mini->stdin_saved != -1)
+				close(mini->stdin_saved);
+			mini->exit_stat = 1;
+			return (perror("Failed to save STDOUT"), 1);
+		}
 	}
 	return (0);
 }
@@ -36,16 +43,27 @@ int	save_fds(t_shell *mini)
 
 int	reset_fds(t_shell *mini)
 {
-	if (dup2(mini->stdin_saved, STDIN_FILENO) == -1
-		|| dup2(mini->stdout_saved, STDOUT_FILENO) == -1)
+	if (mini->stdin_saved != -1)
 	{
-		perror("Failed to restore original FDs");
+		if (dup2(mini->stdin_saved, STDIN_FILENO) == -1)
+		{
+			perror("Failed to restore original STDIN");
+			close(mini->stdin_saved);
+			mini->exit_stat = 1;
+			return (1);
+		}
 		close(mini->stdin_saved);
-		close(mini->stdout_saved);
-		mini->exit_stat = 1;
-		return (1);
 	}
-	close(mini->stdin_saved);
-	close(mini->stdout_saved);
+	if (mini->stdout_saved != -1)
+	{
+		if (dup2(mini->stdout_saved, STDOUT_FILENO) == -1)
+		{
+			perror("Failed to restore original STDOUT");
+			close(mini->stdout_saved);
+			mini->exit_stat = 1;
+			return (1);
+		}
+		close(mini->stdout_saved);
+	}
 	return (0);
 }
