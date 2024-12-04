dfb16f34fd5ad4907802ec9a7ea9b9740daa7511
Heidi E <henbuska@student.hive.fi>
2024-12-04 15:36:00 +0200

    Remove comment functions

--- sources/executor/child_process_utils.c
+++ sources/executor/child_process_utils.c
@@ -6,43 +6,18 @@
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 09:58:47 by henbuska          #+#    #+#             */
-/*   Updated: 2024/12/03 14:30:57 by henbuska         ###   ########.fr       */
+/*   Updated: 2024/12/04 15:34:19 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
 int		dup_input(t_shell *mini, t_cmd *cmd, int i);
-//int	dup_output(t_cmd *cmd, int pipe_fd[2], int count, int i);
 int		dup_output(t_shell *mini, t_cmd *cmd, int count, int i);
 int		dup2_and_close(int old_fd, int new_fd);
 
 // Duplicates input from fd_in if there is a redirection or from previous pipe
 // Redirection takes precedence over pipe
-
-/*int	dup_input(t_shell *mini, t_cmd *cmd, int i)
-{
-	if (cmd->fd_in != STDIN_FILENO)
-	{
-		if (dup2_and_close(cmd->fd_in, STDIN_FILENO))
-		{
-			perror("dup2 for input redirection");
-			cmd->cmd_exit = 1;
-			return (1);
-		}
-	}
-	else if (i > 0) // Middle or last command
-	{
-		if (dup2_and_close(mini->prev_pipe, STDIN_FILENO) == -1)
-		{
-			perror("dup2 for pipe input");
-			cmd->cmd_exit = 1;
-			return (1);
-		}
-		//printf("Pipe input handled for cmd[%d].\n", i);
-	}
-	return (0);
-} */
 
 int	dup_input(t_shell *mini, t_cmd *cmd, int i)
 {
@@ -57,7 +32,7 @@ int	dup_input(t_shell *mini, t_cmd *cmd, int i)
 		if (i > 0)
 		close(mini->pipes[i - 1][0]);
 	}
-	else if (i > 0) // Middle or last command
+	else if (i > 0)
 	{
 		if (dup2_and_close(mini->pipes[i - 1][0], STDIN_FILENO) == -1)
 		{
@@ -65,7 +40,6 @@ int	dup_input(t_shell *mini, t_cmd *cmd, int i)
 			cmd->cmd_exit = 1;
 			return (1);
 		}
-		//printf("Pipe input handled for cmd[%d].\n", i);
 	}
 	return (0);
 }
@@ -73,39 +47,10 @@ int	dup_input(t_shell *mini, t_cmd *cmd, int i)
 // Duplicates output to fd if there is a redirection or to write end of pipe
 // Redirection takes precedence over pipe
 
-/*int	dup_output(t_cmd *cmd, int pipe_fd[2], int count, int i)
-{
-	if (cmd->fd_out != STDOUT_FILENO)
-	{
-		//printf("Redirecting output: cmd->fd_out = %d\n", cmd->fd_out);
-		if (dup2_and_close(cmd->fd_out, STDOUT_FILENO))
-		{
-			perror("dup2 for output redirection");
-			cmd->cmd_exit = 1;
-			return (1);
-		}
-		if (i < count - 1)
-			close(pipe_fd[1]);
-	}
-	else if (i < count - 1)
-	{
-		printf("Using pipe for output in cmd %d: pipe_fd[1] = %d\n", i, pipe_fd[1]);
-		if (dup2_and_close(pipe_fd[1], STDOUT_FILENO))
-		{
-			perror("dup2 for output to pipe");
-			cmd->cmd_exit = 1;
-			return (1);
-		}
-		//close(pipe_fd[0]);
-	}
-	return (0);
-} */
-
 int	dup_output(t_shell *mini, t_cmd *cmd, int count, int i)
 {
 	if (cmd->fd_out != STDOUT_FILENO)
 	{
-		//printf("Redirecting output: cmd->fd_out = %d\n", cmd->fd_out);
 		if (dup2_and_close(cmd->fd_out, STDOUT_FILENO))
 		{
 			perror("dup2 for output redirection");
@@ -117,14 +62,12 @@ int	dup_output(t_shell *mini, t_cmd *cmd, int count, int i)
 	}
 	else if (i < count - 1)
 	{
-		//printf("Using pipe for output in cmd %d: pipe_fd[1] = %d\n", i, pipe_fd[1]);
 		if (dup2_and_close(mini->pipes[i][1], STDOUT_FILENO))
 		{
 			perror("dup2 for output to pipe");
 			cmd->cmd_exit = 1;
 			return (1);
 		}
-		//close(mini->pipes[i][0]);
 	}
 	return (0);
 }
