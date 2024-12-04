6cc5d2be9dc0eb53afdf006a93bfd2f755a9bbeb
Heidi E <henbuska@student.hive.fi>
2024-12-04 15:39:02 +0200

    Remove free(mini), edit to handle quotes in filename

--- sources/utils/exit_handler.c
+++ sources/utils/exit_handler.c
@@ -6,30 +6,11 @@
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 11:43:47 by henbuska          #+#    #+#             */
-/*   Updated: 2024/12/03 18:57:25 by henbuska         ###   ########.fr       */
+/*   Updated: 2024/12/04 15:26:57 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-/*void	exit_for_pipes(t_shell *mini, int i, int exit_status)
-{
-	if (mini->prev_pipe != -1)
-		close(mini->prev_pipe);
-	if (mini->cmd_count > 0 && i < mini->cmd_count - 1)
-		close(mini->pipes[i][1]);
-	if (mini->cmds[i]->fd_out != STDOUT_FILENO && mini->cmds[i]->fd_out != -1)
-		close(mini->cmds[i]->fd_out);
-	if (mini->cmds[i]->fd_in != STDIN_FILENO && mini->cmds[i]->fd_in != -1)
-		close(mini->cmds[i]->fd_in);
-	clean_env(mini->env, mini->pending);
-	clean_cmds(mini->cmds);
-	free(mini->pids);
-	mini->pids = NULL;
-	free(mini);
-	mini = NULL;
-	exit (exit_status);
-} */
 
 void	exit_handler(t_shell *mini, int i, int exit_status)
 {
@@ -64,11 +45,11 @@ void	exit_for_success(t_shell *mini, int i, int exit_status)
 	clean_env(mini->env, mini->pending);
 	clean_cmds(mini->cmds);
 	free(mini->pids);
+	mini->pids = NULL;
 	if (mini->cmd_count > 1)
 		ft_free_int_arr_with_size(mini->pipes, mini->cmd_count - 1);
-	mini->pids = NULL;
-	free(mini);
-	mini = NULL;
+	//free(mini);
+	//mini = NULL;
 	exit (exit_status);
 }
 
@@ -78,8 +59,8 @@ void	exit_for_single_cmd(t_shell *mini, int exit_status)
 	clean_cmds(mini->cmds);
 	free(mini->pids);
 	mini->pids = NULL;
-	free(mini);
-	mini = NULL;
+	//free(mini);
+	//mini = NULL;
 	exit (exit_status);
 }
 
