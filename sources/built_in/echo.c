ff11b0789f963aa1f903d4b3140ff03cb676dd49
Heidi E <henbuska@student.hive.fi>
2024-12-05 18:43:53 +0200

    Missing correct parenthesis

--- sources/built_in/echo.c
+++ sources/built_in/echo.c
@@ -6,7 +6,7 @@
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 18:10:11 by nzharkev          #+#    #+#             */
-/*   Updated: 2024/12/05 18:31:28 by henbuska         ###   ########.fr       */
+/*   Updated: 2024/12/05 18:38:29 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,24 @@
 /*Prints the given arguments to the standard output.
 Supports the -n flag to suppress the newline at the end.*/
 
-int	built_echo(t_cmd *cmd)
+int	built_echo(t_shell *mini, t_cmd *cmd)
 {
 	int	nl;
 	int	i;
 
 	nl = 1;
 	i = 1;
-	if (cmd->args[1] && (ft_strcmp(cmd->args[1], "-n") == 0) || ft_strcmp(cmd->args[1], "$?") == 0)
+	if ((cmd->args[1] && ft_strcmp(cmd->args[1], "-n") == 0) || ft_strcmp(cmd->args[1], "$?") == 0)
 	{
 		if (ft_strcmp(cmd->args[1], "-n") == 0)
 		{
 			nl = 0;
 			i++;
+		}
+		else
+		{
+			printf("%d\n", mini->exit_stat);
+			return (0);
 		}
 	}
 	while (cmd->args[i])
