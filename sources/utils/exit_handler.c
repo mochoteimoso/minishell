36086a1359d98ea78603f348e0197c88d9f07514
Heidi E <henbuska@student.hive.fi>
2024-12-12 21:23:16 +0200

    Add a new cleaner_for_main_success function

--- sources/utils/exit_handler.c
+++ sources/utils/exit_handler.c
@@ -6,7 +6,7 @@
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 11:43:47 by henbuska          #+#    #+#             */
-/*   Updated: 2024/12/05 17:20:20 by henbuska         ###   ########.fr       */
+/*   Updated: 2024/12/12 11:58:36 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,10 +68,56 @@ void	exit_for_single_cmd(t_shell *mini, int exit_status)
 
 void	cleaner_for_main(t_shell *mini)
 {
+	int	i;
+
 	clean_cmds(mini->cmds);
-	free(mini->pids);
-	mini->pids = NULL;
+	if (mini->pids)
+	{
+		free(mini->pids);
+		mini->pids = NULL;
+	}
+	if (mini->cmd_count > 1 && mini->pipes)
+	{
+		i = 0;
+		while (i < mini->cmd_count - 1)
+		{
+			if (mini->pipes[i][0] != -1)
+			{
+				if (mini->pipes[i][0] > 0)
+					close(mini->pipes[i][0]);
+			}
+			if (mini->pipes[i][1] != -1)
+			{
+				if (mini->pipes[i][1] > 1)
+					close(mini->pipes[i][1]);
+			}
+			free(mini->pipes[i]);
+			i++;
+		}
+		free(mini->pipes);
+		mini->pipes = NULL;
+	}
+}
+
+void	cleaner_for_main_success(t_shell *mini)
+{
+	int	i;
+
+	i = mini->cmd_count - 2;
+	clean_cmds(mini->cmds);
+	if (mini->pids)
+	{
+		free(mini->pids);
+		mini->pids = NULL;
+	}
+	if (mini->cmd_count > 1 && mini->pipes)
+	{
+		if (mini->pipes[i][0] != -1)
+		{
+			if (mini->pipes[i][0] > 0)
+				close(mini->pipes[i][0]);
+		}
+	}
 	if (mini->cmd_count > 1)
 		ft_free_int_arr_with_size(mini->pipes, mini->cmd_count - 1);
 }
-
