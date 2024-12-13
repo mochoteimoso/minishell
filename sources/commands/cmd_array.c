371d9678a5209b6c91050620f57b5f12936a438b
Heidi E <henbuska@student.hive.fi>
2024-12-13 20:02:50 +0200

    Add cmd_index to struct

--- sources/commands/cmd_array.c
+++ sources/commands/cmd_array.c
@@ -6,7 +6,7 @@
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 19:09:13 by henbuska          #+#    #+#             */
-/*   Updated: 2024/12/11 18:27:10 by henbuska         ###   ########.fr       */
+/*   Updated: 2024/12/13 14:30:18 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,6 +82,7 @@ void	initialize_command_struct(t_cmd *cmd)
 {
 	cmd->command = NULL;
 	cmd->cmd_path = NULL;
+	cmd->cmd_index = 0;
 	cmd->args = NULL;
 	cmd->args_count = 0;
 	cmd->redir_head = NULL;
