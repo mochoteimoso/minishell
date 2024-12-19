ab9e513272c8758e3b87500353cdb92b24d96140
Heidi E <henbuska@student.hive.fi>
2024-12-19 20:51:10 +0200

    Move functions to new file

---
+++ sources/syntax/trailing_pipe.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   trailing_pipe.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 12:32:24 by henbuska          #+#    #+#             */
+/*   Updated: 2024/12/19 13:11:42 by henbuska         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+static char	*join_and_free(char *input, char *additional_input)
+{
+	char	*updated_input;
+
+	updated_input = ft_strjoin(input, additional_input);
+	free(additional_input);
+	if (!updated_input)
+	{
+		perror("malloc");
+		free(input);
+		return (NULL);
+	}
+	free(input);
+	return (updated_input);
+}
+
+/* Handles trailing pipe by getting additional input from user
+and joining that to the original input string */
+
+char	*handle_trailing_pipe(char *input)
+{
+	char	*additional_input;
+
+	additional_input = NULL;
+	while (1)
+	{
+		additional_input = readline(">");
+		if (!additional_input)
+		{
+			perror("readline error");
+			free(input);
+			return (NULL);
+		}
+		if (check_non_whitespace(additional_input))
+			return (join_and_free(input, additional_input));
+		free(additional_input);
+	}
+}
---
+++ sources/utils/cleaners.c
@@ -0,0 +1,135 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cleaners.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 13:18:31 by henbuska          #+#    #+#             */
+/*   Updated: 2024/12/19 13:53:22 by henbuska         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	mini_cleaner(t_shell *mini)
+{
+	if (mini->env)
+		clean_env(mini->env, NULL);
+	if (mini->pending)
+		ft_free_array(mini->pending);
+	if (mini->cmds)
+		clean_cmds(mini->cmds);
+	if (mini->pids)
+		free(mini->pids);
+	if (mini->cmd_count > 1)
+		ft_free_int_arr_with_size(mini->pipes, mini->cmd_count - 1);
+}
+
+static void	free_and_close_pipes(t_shell *mini)
+{
+	int	i;
+
+	i = 0;
+	while (i < mini->cmd_count - 1)
+	{
+		if (mini->pipes[i][0] > 0)
+			close(mini->pipes[i][0]);
+		if (mini->pipes[i][1] > 1)
+			close(mini->pipes[i][1]);
+		free(mini->pipes[i]);
+		i++;
+	}
+	free(mini->pipes);
+	mini->pipes = NULL;
+}
+
+void	cleaner_for_failure(t_shell *mini)
+{
+	clean_cmds(mini->cmds);
+	if (mini->pids)
+	{
+		free(mini->pids);
+		mini->pids = NULL;
+	}
+	if (mini->cmd_count > 1 && mini->pipes)
+		free_and_close_pipes(mini);
+}
+
+/*void	cleaner_for_main(t_shell *mini)
+{
+	int	i;
+
+	clean_cmds(mini->cmds);
+	if (mini->pids)
+	{
+		free(mini->pids);
+		mini->pids = NULL;
+	}
+	if (mini->cmd_count > 1 && mini->pipes)
+	{
+		i = 0;
+		while (i < mini->cmd_count - 1)
+		{
+			if (mini->pipes[i][0] != -1)
+			{
+				if (mini->pipes[i][0] > 0)
+					close(mini->pipes[i][0]);
+			}
+			if (mini->pipes[i][1] != -1)
+			{
+				if (mini->pipes[i][1] > 1)
+					close(mini->pipes[i][1]);
+			}
+			free(mini->pipes[i]);
+			i++;
+		}
+		free(mini->pipes);
+		mini->pipes = NULL;
+	}
+} */
+
+void	cleaner_for_success(t_shell *mini)
+{
+	int	i;
+
+	i = mini->cmd_count - 2;
+	clean_cmds(mini->cmds);
+	if (mini->pids)
+	{
+		free(mini->pids);
+		mini->pids = NULL;
+	}
+	if (mini->cmd_count > 1 && mini->pipes)
+	{
+		if (mini->pipes[i][0] != -1)
+		{
+			if (mini->pipes[i][0] > 0)
+				close(mini->pipes[i][0]);
+		}
+	}
+	if (mini->cmd_count > 1)
+		ft_free_int_arr_with_size(mini->pipes, mini->cmd_count - 1);
+}
+
+void	clean_cmds(t_cmd **cmds)
+{
+	int	i;
+
+	i = 0;
+	while (cmds[i])
+	{
+		free(cmds[i]->segment);
+		if (cmds[i]->command != NULL)
+			free(cmds[i]->command);
+		if (cmds[i]->cmd_path && cmds[i]->cmd_path != cmds[i]->command)
+			free(cmds[i]->cmd_path);
+		ft_free_array(cmds[i]->args);
+		if (cmds[i]->redir_head)
+			clean_redir(cmds[i]->redir_head);
+		free(cmds[i]);
+		i++;
+	}
+	free(cmds);
+	cmds = NULL;
+}
