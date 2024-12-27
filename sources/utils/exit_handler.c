4e05e5d7ce79ee5f507ad1d656ae2458b9d8ca79
Heidi E <henbuska@student.hive.fi>
2024-12-27 21:08:47 +0200

    Combine if condition in exit_for_failure

--- sources/utils/exit_handler.c
+++ sources/utils/exit_handler.c
@@ -6,28 +6,26 @@
 /*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 11:43:47 by henbuska          #+#    #+#             */
-/*   Updated: 2024/12/23 19:03:17 by henbuska         ###   ########.fr       */
+/*   Updated: 2024/12/27 20:53:33 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+void	exit_for_failure(t_shell *mini, int i, int exit_status);
+void	exit_for_success(t_shell *mini, int i, int exit_status);
+void	exit_for_single_cmd(t_shell *mini, int exit_status);
+void	close_all_pipes(t_shell *mini);
+void	hd_free(t_expand *arg, char *expan);
+
 void	exit_for_failure(t_shell *mini, int i, int exit_status)
 {
-	int	j;
-
-	j = 0;
-	while (j < mini->cmd_count - 1)
-	{
-		close(mini->pipes[j][0]);
-		close(mini->pipes[j][1]);
-		j++;
-	}
-	if (mini->cmd_count >= 2 && i < mini->cmd_count - 1)
+	close_all_pipes(mini);
+	/*if (mini->cmd_count >= 2 && i < mini->cmd_count - 1)
 	{
 		close(mini->pipes[i][0]);
 		close(mini->pipes[i][1]);
-	}
+	}*/
 	if (mini->cmds[i]->fd_out > 2)
 		close(mini->cmds[i]->fd_out);
 	if (mini->cmds[i]->fd_in > 2)
@@ -73,4 +71,17 @@ void	hd_free(t_expand *arg, char *expan)
 {
 	free(arg->value);
 	free(expan);
+}
+
+void	close_all_pipes(t_shell *mini)
+{
+	int	j;
+
+	j = 0;
+	while (j < mini->cmd_count - 1)
+	{
+		close(mini->pipes[j][0]);
+		close(mini->pipes[j][1]);
+		j++;
+	}
 }
